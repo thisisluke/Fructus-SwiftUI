ffa5d019389df695571ef3230abb895d31435094
thisisluke <luke.liu.au@outlook.com>
2020-07-26 21:59:11 +1000

    init project

---
+++ Fructus-SwiftUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Fructus-SwiftUI
+//
+//  Created by luke2015 on 26/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fructus-SwiftUI/Fructus_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fructus_SwiftUIApp.swift
+//  Fructus-SwiftUI
+//
+//  Created by luke2015 on 26/7/20.
+//
+
+import SwiftUI
+
+@main
+struct Fructus_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
